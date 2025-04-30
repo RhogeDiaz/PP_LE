3c53e5c051f768e5b545613d92a0b29e54a5b35a
RhogeDiaz <rvadiaz09@gmail.com>
2025-04-30 21:57:46 +0800

    added netbeans project

---
+++ le/src/main/java/com/mycompany/testapp1/TestApp1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.testapp1;
+
+/**
+ *
+ * @author rvadi
+ */
+public class TestApp1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
