66ac0d5fd2a302129169d80d5824bbeb4a07211d
Ceres2023 <cyreldahan@gmail.com>
2025-05-01 22:10:28 +0800

    for testing na change

--- src/main/java/com/mycompany/le_project/main.java
+++ src/main/java/com/mycompany/le_project/main.java
@@ -2,8 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.mycompany.testapp1;
+package com.mycompany.le_project;
 
+import java.util.Scanner;
 /**
  *
  * @author rvadi
@@ -12,5 +13,12 @@ public class main{
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+       
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Choose one: ");
+        String choice = input.nextLine();
+        
+        System.out.println("You chosen " + choice);
     }
 }
